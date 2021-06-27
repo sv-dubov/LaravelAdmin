ab510f10f862b44617bc98971067dc55d0752efa
Sviatoslav <sv_dubov@ukr.net>
2021-06-27 12:50:04 +0300

    Made users migration and seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
+        User::factory()->count(20)->create();
     }
 }
