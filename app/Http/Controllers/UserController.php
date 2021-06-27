45de352881331937aaba1ecff240425b6d96dd83
Sviatoslav <sv_dubov@ukr.net>
2021-06-27 14:49:04 +0300

    Made User's custom requests

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserUpdateRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -21,7 +22,7 @@ class UserController extends Controller
         return $user;
     }
 
-    public function store(Request $request)
+    public function store(UserCreateRequest $request)
     {
         //$user = User::create($request->all());
         $user = User::create(
@@ -31,7 +32,7 @@ class UserController extends Controller
         return response($user, 201);
     }
 
-    public function update(Request $request, $id)
+    public function update(UserUpdateRequest $request, $id)
     {
         $user = User::find($id);
         $user->update($request->only('first_name', 'last_name', 'email'));
